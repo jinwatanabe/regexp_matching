c8bc74430241448dbc87a2a83659ecdbf6462863
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-02-16 21:32:25 +0900

    change API Port

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -5,7 +5,7 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     let app = create_app();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
