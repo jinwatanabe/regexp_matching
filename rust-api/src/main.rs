fc12cdac334e9c485f5cdeb68f3ae9a49ced6e68
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-02-16 21:37:56 +0900

    Add Logging

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,12 +1,16 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, env};
 use axum::{routing::get, Router};
 
 
 #[tokio::main]
 async fn main() {
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
     let app = create_app();
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-
+    tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
