8eabe3fad87ac5457c34856772a27389498f17ba
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-02-16 21:30:05 +0900

    HelloWorld API create

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::{routing::get, Router};
+
+
+#[tokio::main]
+async fn main() {
+    let app = create_app();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn create_app() -> Router {
+    Router::new().route("/", get(root))
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, Response, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let res = create_app().oneshot(req).await.unwrap();
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        assert_eq!(body, "Hello, World!")
+    }
+
 }
